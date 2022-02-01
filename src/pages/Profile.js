9f206f685090534c879789bd6884c49adf922fa7
MatheusMena <matheusmena1@hotmail.com>
2022-02-01 16:22:08 -0300

    requisito 13:13. Crie a exibição de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Profile extends Component {
-  render() {
-    return (
-      <div data-testid="page-profile">
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      load: false,
+      userName: '',
+      email: '',
+      description: '',
+    };
+    this.handleProfile = this.handleProfile.bind(this);
   }
+
+  componentDidMount() {
+    this.handleProfile();
+  }
+
+ handleProfile = async () => {
+   this.setState({
+     load: true,
+   });
+   const user = await getUser();
+   console.log(user);
+   this.setState({
+     userName: user.name,
+     email: user.email,
+     description: user.description,
+     image: user.image,
+     load: false,
+   });
+ };
+
+ render() {
+   const { userName, email, description, image, load } = this.state;
+   return (
+     <div data-testid="page-profile">
+       <Header />
+       {load ? (<Loading />
+       ) : (
+         <div>
+           <div>
+             <p>{email}</p>
+             <p>{description}</p>
+             <p>{userName}</p>
+             <Link to="/profile/edit">Editar perfil</Link>
+             <img src={ image } data-testid="profile-image" alt="profile-pic" />
+           </div>
+
+         </div>
+       )}
+       ;
+     </div>
+   );
+ }
 }
